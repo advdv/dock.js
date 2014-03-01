71d3c7100c95d58ebb1baedbf9c8a00a1d0d4359
Ad van der Veer <advanderveer@gmail.com>
2014-03-01 16:31:19 +0100

    no longer dock's responsibility to handle exceptions

--- docs/examples/build_image.js
+++ docs/examples/build_image.js
@@ -3,14 +3,24 @@ var Dock = require('../../index.js'); // --> npm install dock.js
 var Docker = require('dockerode'); // --> npm install dockerode
 var winston = require('winston'); // --> npm install winston
 
+//create client
 var client = new Docker({host: 'http://172.12.8.150', port: 4243});
 
-var image = new Dock.Image(client, __dirname + '/sandbox', 'sandbox:0.3', {nocache: true});
+//create api
 var dock = new Dock(client);
 
+//display logging in console
 dock.logger.add(winston.transports.Console, {colorize: true}); 
-image.logger.add(winston.transports.Console, {colorize: true}); 
+ 
+//define images
+dock.image('sandbox:0.3', __dirname + '/sandbox', {nocache: true});
 
+//define services
 dock.service('src', [], 'sandbox:0.3', {Cmd: 'pwd'});
 
-image.build();
+//build then start
+dock.build()
+    .then(dock.start)
+    .catch(function(err){
+      dock.logger.error(err.message);
+    });
