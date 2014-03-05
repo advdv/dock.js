2d8e2b970e0cafc995b4da3918147475f07c54f8
Ad van der Veer <advanderveer@gmail.com>
2014-03-05 15:41:32 +0100

    added the ability to prefix dock instances to avoid container name collistion for all services in the instance

--- test/dock_test.js
+++ test/dock_test.js
@@ -24,6 +24,7 @@ describe('Dock()', function(){
     
     docker = stubDockerode(new Docker({host: 'http://' + parts.hostname, port: parts.port}));
     dock = new Dock(docker);
+    dock.prefix  = 'test_';
   });
 
   it('should construct as expected', function(){
@@ -34,6 +35,7 @@ describe('Dock()', function(){
     c.should.have.property('has').and.be.instanceOf(Function);
     c.should.have.property('add').and.be.instanceOf(Function);
     
+    c.should.have.property('prefix').and.equal('');
     c.should.have.property('repository').and.be.instanceOf(Repository);
     c.should.have.property('docker').and.equal(docker);
 
@@ -44,18 +46,40 @@ describe('Dock()', function(){
     c = new Dock(docker, l);
     c.logger.should.equal(l);
 
-    //invalid logger, initiated default
-    c = new Dock(docker, 'bogus');
+    //specify prefix, should have defautl logger 
+    c = new Dock(docker, 'wkmb-');
     c.should.have.property('logger').and.be.instanceOf(winston.Logger);
+
+    c.prefix.should.equal('wkmb-');
+
   });
 
   describe('.add(), .has(), .get()', function(){
     it('should add services correctly', function(){
       var s = {};
 
+      dock.prefix  = '';
       dock.add('phpfpm', s);
       dock.has('phpfpm').should.equal(true);
       dock.get('phpfpm').should.equal(s);
+      dock.get('phpfpm').name.should.equal('phpfpm');
+      
+      (function(){
+        dock.get('bogus');
+      }).should.throw(/Could not retrieve service/);
+            
+      dock.has('bogus').should.equal(false);
+    });
+
+    it('should add services correctly with prefix', function(){
+      var s = {};
+
+      dock.prefix  = 'test_';
+
+      dock.add('phpfpm', s);
+      dock.has('phpfpm').should.equal(true);
+      dock.get('phpfpm').should.equal(s);
+      dock.get('phpfpm').name.should.equal('test_phpfpm');
       
       (function(){
         dock.get('bogus');
@@ -79,6 +103,8 @@ describe('Dock()', function(){
       var s2 = dock.service('nginx', ['phpfpm']);
       var s3 = dock.service('data', ['bogus']);
       var s4 = dock.service('src', [], 'busybox:latest', {Cmd: 'ls'}); //shorthand
+
+      s4.containers[0].name.should.equal('test_src_0');
 
       s2.should.be.an.instanceOf(Service);
       s2.dependencies.length.should.equal(1);
@@ -118,6 +144,24 @@ describe('Dock()', function(){
   });
 
   describe('.analyse()', function(){
+
+    it('should be able to retrieve root services with prefix', function(){
+
+      var d = new Dock(docker, 'p_');
+      
+      d.service('f'); //lone service counts as root
+
+      d.service('y');
+      d.service('x', ['y']); // x -> y
+      
+      d.service('a', ['b']); // a -> b
+      d.service('b', ['c']);   // b -> c  
+      d.service('c', []);  // c
+
+      d.getRootServices().should.eql(['p_f', 'p_x', 'p_a']);
+    });
+
+
     
     it('should be able to retrieve root services', function(){
 
@@ -165,6 +209,7 @@ describe('Dock()', function(){
     beforeEach(function(){
       memTrans = new winston.transports.Memory();
       d = new Dock(docker);
+      d.prefix = 'fix_';
 
       d.logger.add(memTrans, {}, true);
       d.service('src').container('busybox:latest');
@@ -183,11 +228,11 @@ describe('Dock()', function(){
         //test started order
         var o = memTrans.writeOutput;
 
-        (o.indexOf('info: [src] started!')).should.be.lessThan(o.indexOf('info: [php] started!'));
-        (o.indexOf('info: [php] started!')).should.be.lessThan(o.indexOf('info: [data] started!'));
-        (o.indexOf('info: [php] started!')).should.be.lessThan(o.indexOf('info: [http] started!'));
-        (o.indexOf('info: [http] started!')).should.be.lessThan(o.indexOf('info: [data] started!'));
-        o.indexOf('info: [sql] started!').should.equal(-1);
+        (o.indexOf('info: [fix_src] started!')).should.be.lessThan(o.indexOf('info: [fix_php] started!'));
+        (o.indexOf('info: [fix_php] started!')).should.be.lessThan(o.indexOf('info: [fix_data] started!'));
+        (o.indexOf('info: [fix_php] started!')).should.be.lessThan(o.indexOf('info: [fix_http] started!'));
+        (o.indexOf('info: [fix_http] started!')).should.be.lessThan(o.indexOf('info: [fix_data] started!'));
+        o.indexOf('info: [fix_sql] started!').should.equal(-1);
 
         done();
       });
@@ -211,17 +256,17 @@ describe('Dock()', function(){
         var o = memTrans.writeOutput;
 
         //busybox and sql can start concurrently: sql starts while src is'nt started yet
-        (o.indexOf('info: [sql] starting...')).should.be.lessThan(o.indexOf('info: [src] started!'));
-        (o.indexOf('info: [src] starting...')).should.be.lessThan(o.indexOf('info: [sql] starting...'));
+        (o.indexOf('info: [fix_sql] starting...')).should.be.lessThan(o.indexOf('info: [fix_src] started!'));
+        (o.indexOf('info: [fix_src] starting...')).should.be.lessThan(o.indexOf('info: [fix_sql] starting...'));
 
         //general order
-        (o.indexOf('info: [src] started!')).should.be.lessThan(o.indexOf('info: [php] started!'));
-        (o.indexOf('info: [php] started!')).should.be.lessThan(o.indexOf('info: [data] started!'));
-        (o.indexOf('info: [php] started!')).should.be.lessThan(o.indexOf('info: [http] started!'));
-        (o.indexOf('info: [http] started!')).should.be.lessThan(o.indexOf('info: [data] started!'));
+        (o.indexOf('info: [fix_src] started!')).should.be.lessThan(o.indexOf('info: [fix_php] started!'));
+        (o.indexOf('info: [fix_php] started!')).should.be.lessThan(o.indexOf('info: [fix_data] started!'));
+        (o.indexOf('info: [fix_php] started!')).should.be.lessThan(o.indexOf('info: [fix_http] started!'));
+        (o.indexOf('info: [fix_http] started!')).should.be.lessThan(o.indexOf('info: [fix_data] started!'));
         
         //this also starts independant sql service
-        o.indexOf('info: [sql] started!').should.not.equal(-1);
+        o.indexOf('info: [fix_sql] started!').should.not.equal(-1);
         done();
       });
 
